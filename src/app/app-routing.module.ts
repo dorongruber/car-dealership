e8f3435317d8e790dec74d0b896fd76c3754f967
dorongruber <doron.gruber@gmail.com>
2024-01-26 10:44:02 +0200

    create not found page and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing', pathMatch: 'full'},
   {path: 'landing', loadChildren: () => import("./landing/landing.module").then(m => m.LandingModule) },
   {path: 'car-request', loadChildren: () => import("./car-request/car-request.module").then(m => m.CarRequestModule) },
   {path: 'dashboard', loadChildren: () => import("./dashboard/dashboard.module").then(m => m.DashboardModule) },
+  
+  {path: 'page-not-found', component: NotfoundComponent},
+  {path: '**', redirectTo: 'page-not-found' },
 ];
 
 @NgModule({
---
+++ src/app/notfound/notfound.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.scss'],
+  standalone: true,
+})
+export class NotfoundComponent {
+
+}

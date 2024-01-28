edadef5e65f091263712da80aa924628a6046d18
dorongruber <doron.gruber@gmail.com>
2024-01-28 11:44:50 +0200

    add slide in basic sliding animation for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,27 @@ import { NotfoundComponent } from './notfound/notfound.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing', pathMatch: 'full'},
-  {path: 'landing', loadChildren: () => import("./landing/landing.module").then(m => m.LandingModule) },
-  {path: 'car-request', loadChildren: () => import("./car-request/car-request.module").then(m => m.CarRequestModule) },
-  {path: 'dashboard', loadChildren: () => import("./dashboard/dashboard.module").then(m => m.DashboardModule) },
+  {
+    path: 'landing', 
+    loadChildren: () => import("./landing/landing.module").then(m => m.LandingModule),
+    data: { animation: "LandingPage"},
+  },
+  {
+    path: 'car-request', 
+    loadChildren: () => import("./car-request/car-request.module").then(m => m.CarRequestModule),
+    data: { animation: "FormPage"},
+  },
+  {
+    path: 'dashboard', 
+    loadChildren: () => import("./dashboard/dashboard.module").then(m => m.DashboardModule),
+    data: { animation: "DashboardModule"},
+  },
   
-  {path: 'page-not-found', component: NotfoundComponent},
+  {
+    path: 'page-not-found', 
+    component: NotfoundComponent,
+    data: { animation: "NotFoundPage"},
+  },
   {path: '**', redirectTo: 'page-not-found' },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ChildrenOutletContexts } from '@angular/router';
+import { slideInAnimation } from './routing-animations/slide-in-animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    slideInAnimation
+  ]
 })
 export class AppComponent {
   title = 'car-dealership';
+
+  constructor(private contexts: ChildrenOutletContexts) {}
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
 }
